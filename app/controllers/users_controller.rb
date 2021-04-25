2a618efc34f38ebb08479af97b6d7468aa9b7aeb
otegami <t.kodama@everyleaf.com>
2021-04-25 17:26:27 +0900

    Added the function of filtering no-login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[index new create]
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
@@ -64,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :crypted_password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
