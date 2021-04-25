c844ea6b126e52cf318a75f294a673cdf2cccf11
otegami <t.kodama@everyleaf.com>
2021-04-25 16:50:54 +0900

    rails g controller UserSessions new create destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root 'books#index'
   resources :users
   resources :books
