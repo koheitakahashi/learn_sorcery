7853a68adac3fd12cd27ec81d34d4838a02eb082
otegami <t.kodama@everyleaf.com>
2021-04-25 17:17:00 +0900

    Implemented Login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root 'books#index'
   resources :users
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'user_sessions#new', as: :login
+  post 'login', to: "user_sessions#create"
+  post 'logout', to: 'user_sessions#destroy', as: :logout
 end
