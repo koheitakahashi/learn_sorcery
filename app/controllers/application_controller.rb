4aaa54e9a4935c82a414553818302111dd8e2316
otegami <t.kodama@everyleaf.com>
2021-05-09 16:05:42 +0900

    Implemented activity logging user list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  helper_method :current_users_list
+
   private
 
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
+  end
+
+  def current_users_list
+    current_users = User.all.select { |user| user.online? }
+    current_users.map { |user| user.email }.join(", ")
   end
 end
